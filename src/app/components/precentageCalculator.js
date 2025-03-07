946c79631a7704ac3b59c13e562293566706e853
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-07 09:02:51 +0100

    set the state

--- src/app/components/precentageCalculator.js
+++ src/app/components/precentageCalculator.js
@@ -1,8 +1,12 @@
 'use state';
 
-import { userSTate } from  'react';
+import { useState } from  'react';
 
 export default function PercentageCalculator() {
+    const [mode, setMode] = useState('find');
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [result, setResult] = useState(null);
 
     return (
         <div>
